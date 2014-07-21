d1914de362d72e74f733ad3ac59fd38be435d992
cmb69 <cmbecker69@gmx.de>
2014-07-21 13:56:35 +0000

    actually added metaconfig.php

---
+++ config/metaconfig.php
@@ -0,0 +1,13 @@
+<?php
+
+$plugin_mcf['realblog']['auto_archive']="bool";
+$plugin_mcf['realblog']['auto_publish']="bool";
+$plugin_mcf['realblog']['comments_function']="bool";
+$plugin_mcf['realblog']['comments_form_protected']="bool";
+$plugin_mcf['realblog']['entries_order']="enum:desc,asc";
+$plugin_mcf['realblog']['show_numberof_entries_bottom']="bool";
+$plugin_mcf['realblog']['show_numberof_entries_top']="bool";
+$plugin_mcf['realblog']['show_read_more_link']="bool";
+$plugin_mcf['realblog']['teaser_multicolumns']="bool";
+
+?>
