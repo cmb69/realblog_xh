cmb69/realblog_xh

commit 891b322dc14263dbc56d4caa38debc7a24391063

Switch from PEAR to PSR-2 CS
We now can prefer private over public members (PEAR CS required
required an underscore prefix for private members, which made it harder
to change the visibility later). We also get rid of the overly verbose
doc blocks required by PEAR CS – actually, code should be self
documenting.

compat.php
@@ -1,12 +1,6 @@
 <?php
 
 /**
- * Backward compatibility.
- *
- * PHP version 5
- *
- * @category  CMSimple_XH
- * @package   Realblog
  * @author    Jan Kanters <jan.kanters@telenet.be>
  * @author    Gert Ebersbach <mail@ge-webdesign.de>
  * @author    Christoph M. Becker <cmbecker69@gmx.de>
@@ -14,20 +8,15 @@
  * @copyright 2010-2014 Gert Ebersbach <http://ge-webdesign.de/>
  * @copyright 2014-2016 Christoph M. Becker <http://3-magi.net/>
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @link      http://3-magi.net/?CMSimple_XH/Realblog_XH
  */
 
 /**
- * Renders the published articles.
- *
- * @param string $options     An option string (options: showsearch).
- * @param string $realBlogCat A category.
- *
- * @return string (X)HTML.
- *
- * @global Realblog\Controller The plugin controller.
+ * @param string $options
+ * @param string $category
+ * @return string
+ * @global Realblog\Controller $_Realblog_controller
  */
-function showrealblog($options = null, $realBlogCat = 'all')
+function showrealblog($options = null, $category = 'all')
 {
     global $_Realblog_controller;
 
@@ -35,27 +24,23 @@ function showrealblog($options = null, $realBlogCat = 'all')
     $arguments = Realblog_getArguments($options);
     if (isset($arguments['showsearch'])) {
         switch (strtolower($arguments['showsearch'])) {
-        case '0':
-        case 'false':
-            $includesearch = false;
-            break;
-        case '1':
-        case 'true':
-            $includesearch = true;
-            break;
+            case '0':
+            case 'false':
+                $includesearch = false;
+                break;
+            case '1':
+            case 'true':
+                $includesearch = true;
+                break;
         }
     }
     return $_Realblog_controller->blog($includesearch, $realBlogCat);
 }
 
 /**
- * Renders the archived articles.
- *
- * @param string $options An option string (options: showsearch).
- *
- * @return string (X)HTML.
- *
- * @global Realblog\Controller The plugin controller.
+ * @param string $options
+ * @return string
+ * @global Realblog\Controller $_Realblog_controller
  */
 function showrealblogarchive($options = null)
 {
@@ -66,27 +51,23 @@ function showrealblogarchive($options = null)
     if (isset($arguments['showsearch'])) {
         $argument = strtolower($arguments['showsearch']);
         switch ($argument) {
-        case '0':
-        case 'false':
-            $includesearch = false;
-            break;
-        case '1':
-        case 'true':
-            $includesearch = true;
-            break;
+            case '0':
+            case 'false':
+                $includesearch = false;
+                break;
+            case '1':
+            case 'true':
+                $includesearch = true;
+                break;
         }
     }
     return $_Realblog_controller->archive($includesearch);
 }
 
 /**
- * Renders the published topics with a link to the blog page from the template.
- *
- * @param string $options An option string (options: realblogpage).
- *
- * @return string (X)HTML.
- *
- * @global Realblog\Controller The plugin controller.
+ * @param string $options
+ * @return string
+ * @global Realblog\Controller $_Realblog_controller
  */
 function realbloglink($options)
 {
@@ -101,10 +82,7 @@ function realbloglink($options)
 }
 
 /**
- * Parses the $arguments string and returns a map of names to values.
- *
- * @param string $arguments An arguments string ('name1=value1,name2=value2').
- *
+ * @param string $arguments
  * @return array
  */
 function Realblog_getArguments($arguments)
@@ -121,11 +99,8 @@ function Realblog_getArguments($arguments)
 }
 
 /**
- * Renders a hyperlink to the newsfeed.
- *
- * @return string (X)HTML.
- *
- * @global Realblog\Controller The plugin controller.
+ * @return string
+ * @global Realblog\Controller $_Realblog_controller
  */
 function realblog_rss_adv()
 {
@@ -135,8 +110,6 @@ function realblog_rss_adv()
 }
 
 /**
- * A dummy function for categories.
- *
  * @return void
  */
 function rbCat()
@@ -145,10 +118,7 @@ function rbCat()
 }
 
 /**
- * Dummy function for compatibility reasons.
- *
  * @return void
- *
  * @deprecated since 3.0beta4
  */
 function commentsMembersOnly()
@@ -156,5 +126,3 @@ function commentsMembersOnly()
     // should be E_USER_DEPRECATED, but that requires PHP >= 5.3 or XH >= 1.6.3
     trigger_error('Function ' . __FUNCTION__ . '() is deprecated', E_USER_NOTICE);
 }
-
-?>

build.xml
@@ -5,7 +5,7 @@
     <fileset id="php-sources" dir=".">
         <include name="classes/*.php"/>
         <include name="index.php"/>
-        <include name="functions.php"/>
+        <include name="compat.php"/>
     </fileset>
 
     <fileset id="js-sources" dir=".">
@@ -25,11 +25,9 @@
         <echo>${help}</echo>
     </target>
 
-    <target name="sniff" description="checks adherence to PEAR CS">
-        <phpcodesniffer standard="PEAR" format="full">
+    <target name="sniff" description="checks adherence to the coding standard">
+        <phpcodesniffer standard="PSR2" format="full">
             <fileset refid="php-sources"/>
-            <fileset refid="unit-tests"/>
-            <!--<fileset refid="attack-tests"/>-->
         </phpcodesniffer>
     </target>
 

classes/AdminController.php
@@ -1,12 +1,6 @@
 <?php
 
 /**
- * The admin controllers.
- *
- * PHP version 5
- *
- * @category  CMSimple_XH
- * @package   Realblog
  * @author    Jan Kanters <jan.kanters@telenet.be>
  * @author    Gert Ebersbach <mail@ge-webdesign.de>
  * @author    Christoph M. Becker <cmbecker69@gmx.de>
@@ -14,32 +8,19 @@
  * @copyright 2010-2014 Gert Ebersbach <http://ge-webdesign.de/>
  * @copyright 2014-2016 Christoph M. Becker <http://3-magi.net/>
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @link      http://3-magi.net/?CMSimple_XH/Realblog_XH
  */
 
 namespace Realblog;
 
 use stdClass;
 
-/**
- * The admin controllers.
- *
- * @category CMSimple_XH
- * @package  Realblog
- * @author   Christoph M. Becker <cmbecker69@gmx.de>
- * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @link     http://3-magi.net/?CMSimple_XH/Realblog_XH
- */
 class AdminController
 {
     /**
-     * Dispatches on administration requests.
-     *
      * @return void
-     *
-     * @global string The value of the <var>admin</var> GP parameter.
-     * @global string The value of the <var>action</var> GP parameter.
-     * @global string The (X)HTML to insert into the contents area.
+     * @global string $admin
+     * @global string $action
+     * @global string $o
      */
     public function dispatch()
     {
@@ -49,36 +30,31 @@ class AdminController
 
         $o .= print_plugin_admin('on');
         switch ($admin) {
-        case '':
-            $o .= $this->renderInfoView();
-            break;
-        case 'plugin_main':
-            $this->handleMainAdministration();
-            break;
-        default:
-            $o .= plugin_admin_common($action, $admin, 'realblog');
+            case '':
+                $o .= $this->renderInfoView();
+                break;
+            case 'plugin_main':
+                $this->handleMainAdministration();
+                break;
+            default:
+                $o .= plugin_admin_common($action, $admin, 'realblog');
         }
     }
 
     /**
-     * Renders the plugin info view.
-     *
-     * @return string (X)HTML.
+     * @return string
      */
-    protected function renderInfoView()
+    private function renderInfoView()
     {
         $view = new InfoView();
         return $view->render();
     }
 
     /**
-     * Handles the main administration.
-     *
      * @return void
-     *
-     * @global string The value of the <var>action</var> GP parameter.
+     * @global string $action
      */
-    protected function handleMainAdministration()
+    private function handleMainAdministration()
     {
         global $action;
 
@@ -86,60 +62,53 @@ class AdminController
     }
 
     /**
-     * Dispatches on the <var>action</var>.
-     *
-     * @param string $action An action.
-     *
+     * @param string $action
      * @return void
-     *
-     * @global string The (X)HTML to insert into the contents area.
+     * @global string $o
      */
-    protected function dispatchOnAction($action)
+    private function dispatchOnAction($action)
     {
         global $o;
 
         switch ($action) {
-        case 'add_realblog':
-        case 'modify_realblog':
-        case 'delete_realblog':
-            $o .= $this->renderArticle();
-            break;
-        case 'do_add':
-            $o .= $this->addArticle();
-            break;
-        case 'do_modify':
-            $o .= $this->modifyArticle();
-            break;
-        case 'do_delete':
-            $o .= $this->deleteArticle();
-            break;
-        case 'batchdelete':
-            $o .= $this->confirmDelete();
-            break;
-        case 'do_delselected':
-            $o .= $this->deleteArticles();
-            break;
-        case 'change_status':
-            $o .= $this->confirmChangeStatus();
-            break;
-        case 'do_batchchangestatus':
-            $o .= $this->changeStatus();
-            break;
-        default:
-            $o .= $this->renderArticles();
+            case 'add_realblog':
+            case 'modify_realblog':
+            case 'delete_realblog':
+                $o .= $this->renderArticle();
+                break;
+            case 'do_add':
+                $o .= $this->addArticle();
+                break;
+            case 'do_modify':
+                $o .= $this->modifyArticle();
+                break;
+            case 'do_delete':
+                $o .= $this->deleteArticle();
+                break;
+            case 'batchdelete':
+                $o .= $this->confirmDelete();
+                break;
+            case 'do_delselected':
+                $o .= $this->deleteArticles();
+                break;
+            case 'change_status':
+                $o .= $this->confirmChangeStatus();
+                break;
+            case 'do_batchchangestatus':
+                $o .= $this->changeStatus();
+                break;
+            default:
+                $o .= $this->renderArticles();
         }
     }
 
     /**
-     * Renders the articles.
-     *
-     * @return string (X)HTML.
-     *
-     * @global array      The configuration of the plugins.
-     * @global array      The localization of the plugins.
-     * @global Controller The plugin controller.
+     * @return string
+     * @global array $plugin_cf
+     * @global array $plugin_tx
+     * @global Controller $_Realblog_controller
      */
-    protected function renderArticles()
+    private function renderArticles()
     {
         global $plugin_cf, $plugin_tx, $_Realblog_controller;
 
@@ -157,34 +126,29 @@ class AdminController
     }
 
     /**
-     * Renders an article.
-     *
-     * @return string (X)HTML.
-     *
-     * @global string     The value of the <var>action</var> GP parameter.
-     * @global Controller The plugin controller.
+     * @return string
+     * @global string $o
+     * @global Controller $_Realblog_controller
      */
-    protected function renderArticle()
+    private function renderArticle()
     {
         global $action, $_Realblog_controller;
 
         init_editor(array('realblog_headline_field', 'realblog_story_field'));
         return $this->form(
-            $_Realblog_controller->getPgParameter('realblogID'), $action
+            $_Realblog_controller->getPgParameter('realblogID'),
+            $action
         );
     }
 
     /**
-     * Adds an article.
-     *
-     * @return string (X)HTML.
-     *
-     * @global string            The page title.
-     * @global array             The localization of the plugins.
-     * @global XH_CSRFProtection The CSRF protector.
-     * @global Controller        The plugin controller.
+     * @return string
+     * @global string $title
+     * @global array $plugin_tx
+     * @global \XH_CSRFProtection $_XH_csrfProtection
+     * @global Controller $_Realblog_controller
      */
-    protected function addArticle()
+    private function addArticle()
     {
         global $title, $plugin_tx, $_XH_csrfProtection, $_Realblog_controller;
 
@@ -197,16 +161,13 @@ class AdminController
     }
 
     /**
-     * Modifies an article.
-     *
-     * @return string (X)HTML.
-     *
-     * @global string            The page title.
-     * @global array             The localization of the plugins.
-     * @global XH_CSRFProtection The CSRF protector.
-     * @global Controller        The plugin controller.
+     * @return string
+     * @global string $title
+     * @global array $plugin_tx
+     * @global \XH_CSRFProtection $_XH_csrfProtection
+     * @global Controller $_Realblog_controller
      */
-    protected function modifyArticle()
+    private function modifyArticle()
     {
         global $title, $plugin_tx, $_XH_csrfProtection, $_Realblog_controller;
 
@@ -219,16 +180,13 @@ class AdminController
     }
 
     /**
-     * Deletes an article.
-     *
-     * @return string (X)HTML.
-     *
-     * @global string            The page title.
-     * @global array             The localization of the plugins.
-     * @global XH_CSRFProtection The CSRF protector.
-     * @global Controller        The plugin controller.
+     * @return string
+     * @global string $title
+     * @global array $plugin_tx
+     * @global \XH_CSRFProtection $_XH_csrfProtection
+     * @global Controller $_Realblog_controller
      */
-    protected function deleteArticle()
+    private function deleteArticle()
     {
         global $title, $plugin_tx, $_XH_csrfProtection, $_Realblog_controller;
 
@@ -241,27 +199,23 @@ class AdminController
     }
 
     /**
-     * Renders the change status confirmation.
-     *
-     * @return string (X)HTML.
+     * @return string
      */
-    protected function confirmChangeStatus()
+    private function confirmChangeStatus()
     {
         $view = new ChangeStatusView();
         return $view->render();
     }
 
     /**
-     * Changes the article status.
+     * @return string
      *
-     * @return string (X)HTML.
-     *
-     * @global string            The page title.
-     * @global array             The localization of the plugins.
-     * @global XH_CSRFProtection The CSRF protector.
-     * @global Controller        The plugin controller.
+     * @global string $title
+     * @global array $plugin_tx
+     * @global \XH_CSRFProtection $_XH_csrfProtection
+     * @global Controller $_Realblog_controller
      */
-    protected function changeStatus()
+    private function changeStatus()
     {
         global $title, $plugin_tx, $_XH_csrfProtection, $_Realblog_controller;
 
@@ -269,7 +223,12 @@ class AdminController
         $ids = $_Realblog_controller->getPgParameter('realblogtopics');
         $status = $_Realblog_controller->getPgParameter('new_realblogstatus');
         if (is_numeric($status) && $status >= 0 && $status <= 2) {
-            $ids = array_map(function ($id) {return (int) $id;}, $ids);
+            $ids = array_map(
+                function ($id) {
+                    return (int) $id;
+                },
+                $ids
+            );
             DB::updateStatusOfArticlesWithIds($ids, $status);
             $title = $plugin_tx['realblog']['tooltip_changestatus'];
             $info = $plugin_tx['realblog']['changestatus_done'];
@@ -282,33 +241,34 @@ class AdminController
     }
 
     /**
-     * Renders the delete confirmation.
-     *
-     * @return string (X)HTML.
+     * @return string
      */
-    protected function confirmDelete()
+    private function confirmDelete()
     {
         $view = new DeleteView();
         return $view->render();
     }
 
     /**
-     * Deletes articles.
+     * @return string
      *
-     * @return string (X)HTML.
-     *
-     * @global string            The page title.
-     * @global array             The localization of the plugins.
-     * @global XH_CSRFProtection The CSRF protector.
-     * @global Controller        The plugin controller.
+     * @global string $title
+     * @global array $plugin_tx
+     * @global \XH_CSRFProtection $_XH_csrfProtection
+     * @global Controller $_Realblog_controller
      */
-    protected function deleteArticles()
+    private function deleteArticles()
     {
         global $title, $plugin_tx, $_XH_csrfProtection, $_Realblog_controller;
 
         $_XH_csrfProtection->check();
         $ids = $_Realblog_controller->getPgParameter('realblogtopics');
-        $ids = array_map(function ($id) {return (int) $id;}, $ids);
+        $ids = array_map(
+            function ($id) {
+                return (int) $id;
+            },
+            $ids
+        );
         DB::deleteArticlesWithIds($ids);
         $title = $plugin_tx['realblog']['tooltip_deleteall'];
         $info = $plugin_tx['realblog']['deleteall_done'];
@@ -316,13 +276,10 @@ class AdminController
     }
 
     /**
-     * Returns an article created from G/P parameters.
-     *
      * @return stdClass
-     *
-     * @global Controller The plugin controller.
+     * @global Controller $_Realblog_controller
      */
-    protected function getArticleFromParameters()
+    private function getArticleFromParameters()
     {
         global $_Realblog_controller;
 
@@ -332,15 +289,15 @@ class AdminController
             $_Realblog_controller->stringToTime(
                 $_Realblog_controller->getPgParameter('realblog_date')
             );
-        $article->title = 
+        $article->title =
             stsl($_Realblog_controller->getPgParameter('realblog_title'));
-        $article->teaser = 
+        $article->teaser =
             stsl($_Realblog_controller->getPgParameter('realblog_headline'));
-        $article->body = 
+        $article->body =
             stsl($_Realblog_controller->getPgParameter('realblog_story'));
         $startDate = $_Realblog_controller->getPgParameter('realblog_startdate');
         if (isset($startDate)) {
-            $article->publishing_date = 
+            $article->publishing_date =
                 $_Realblog_controller->stringToTime($startDate);
         } else {
             $article->publishing_date = 0;
@@ -365,13 +322,10 @@ class AdminController
     }
 
     /**
-     * Returns the current filter statuses.
-     *
      * @return array
-     *
-     * @global Controller The plugin controller.
+     * @global Controller $_Realblog_controller
      */
-    protected function getFilterStatuses()
+    private function getFilterStatuses()
     {
         global $_Realblog_controller;
 
@@ -384,17 +338,13 @@ class AdminController
         return $statuses;
     }
     /**
-     * Writes the required references to the head element.
-     *
      * @return void
-     *
-     * @global array  The paths of system files and folders.
-     * @global string The current language.
-     * @global string The (X)HTML fragment to insert in the head element.
-     *
+     * @global array $pth
+     * @global string $sl
+     * @global string $hjs
      * @todo Check files for existance.
      */
-    protected function useCalendar()
+    private function useCalendar()
     {
         global $pth, $sl, $hjs;
 
@@ -427,17 +377,13 @@ EOT;
     }
 
     /**
-     * Renders the article form.
-     *
-     * @param string $id     An article ID.
-     * @param string $action An action.
-     *
-     * @return string (X)HTML.
-     *
-     * @global string              The page title.
-     * @global array               The localization of the plugins.
+     * @param string $id
+     * @param string $action
+     * @return string
+     * @global string $title
+     * @global array $plugin_tx
      */
-    protected function form($id, $action)
+    private function form($id, $action)
     {
         global $title, $plugin_tx;
 
@@ -471,18 +417,14 @@ EOT;
     }
 
     /**
-     * Displays a confirmation.
-     *
-     * @param string $title A title.
-     * @param string $info  An info message.
-     * @param int    $page  A blog page to return to.
-     *
-     * @return string (X)HTML.
-     *
-     * @global array             The localization of the plugins.
-     * @global string            The script name.
+     * @param string $title
+     * @param string $info
+     * @param int $page
+     * @return string
+     * @global array $plugin_tx
+     * @global string $sn
      */
-    protected function dbconfirm($title, $info, $page)
+    private function dbconfirm($title, $info, $page)
     {
         global $plugin_tx, $sn;
 
@@ -504,7 +446,4 @@ EOT;
         $t .= '</tbody></table></form>';
         return $t;
     }
-
 }
-
-?>

classes/ArchiveView.php
@@ -1,12 +1,6 @@
 <?php
 
 /**
- * The archive views.
- *
- * PHP version 5
- *
- * @category  CMSimple_XH
- * @package   Realblog
  * @author    Jan Kanters <jan.kanters@telenet.be>
  * @author    Gert Ebersbach <mail@ge-webdesign.de>
  * @author    Christoph M. Becker <cmbecker69@gmx.de>
@@ -14,48 +8,33 @@
  * @copyright 2010-2014 Gert Ebersbach <http://ge-webdesign.de/>
  * @copyright 2014-2016 Christoph M. Becker <http://3-magi.net/>
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @link      http://3-magi.net/?CMSimple_XH/Realblog_XH
  */
 
 namespace Realblog;
 
-/**
- * The archive views.
- *
- * @category CMSimple_XH
- * @package  Realblog
- * @author   Christoph M. Becker <cmbecker69@gmx.de>
- * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @link     http://3-magi.net/?CMSimple_XH/Realblog_XH
- */
 class ArchiveView
 {
     /**
-     * The articles.
-     *
      * @var array<stdClass>
      */
     protected $articles;
-    
+
+    /**
+     * @var int
+     */
     protected $articleCount;
 
     /**
-     * The requested year.
-     *
      * @var int
      */
     protected $year;
 
     /**
-     * Initializes a new instance.
-     *
-     * @param array $articles An array of articles.
-     *
-     * @return void
-     *
-     * @global Controller The plugin controller.
+     * @param array<stdClass> $articles
+     * @param int $articleCount
+     * @global Controller $_Realblog_controller
      */
-    public function __construct($articles, $articleCount)
+    public function __construct(array $articles, $articleCount)
     {
         global $_Realblog_controller;
 
@@ -65,12 +44,9 @@ class ArchiveView
     }
 
     /**
-     * Renders the view.
-     *
-     * @return string (X)HTML.
-     *
-     * @global array      The localization of the plugins.
-     * @global Controller The plugin controller.
+     * @return string
+     * @global array $plugin_tx
+     * @global Controller $_Realblog_controller
      */
     public function render()
     {
@@ -109,15 +85,11 @@ class ArchiveView
     }
 
     /**
-     * Returns the localized month name.
-     *
-     * @param int $month A month.
-     *
+     * @param int $month
      * @return string
-     *
-     * @global array The localization of the plugins.
+     * @global array $plugin_tx
      */
-    protected function getMonthName($month)
+    private function getMonthName($month)
     {
         global $plugin_tx;
 
@@ -126,23 +98,21 @@ class ArchiveView
     }
 
     /**
-     * Renders the pagination.
-     *
-     * @param int $back The previous year.
-     * @param int $next The next year.
-     *
-     * @return string (X)HTML.
-     *
-     * @global string     The URL of the current page.
-     * @global array      The localization of the plugins.
-     * @global Controller The plugin controller.
+     * @param int $back
+     * @param int $next
+     * @return string
+     * @global string $su
+     * @global array $plugin_tx
+     * @global Controller $_Realblog_controller
      */
-    protected function renderPagination($back, $next)
+    private function renderPagination($back, $next)
     {
         global $su, $plugin_tx, $_Realblog_controller;
 
         $url = $_Realblog_controller->url(
-            $su, null, array('realblog_year' => $back)
+            $su,
+            null,
+            array('realblog_year' => $back)
         );
         $t = '<div class="realblog_table_paging">'
             . '<a href="' . XH_hsc($url) . '" title="'
@@ -151,7 +121,9 @@ class ArchiveView
         $t .= '<b>' . $plugin_tx['realblog']['archive_year']
             . $this->year . '</b>';
         $url = $_Realblog_controller->url(
-            $su, null, array('realblog_year' => $next)
+            $su,
+            null,
+            array('realblog_year' => $next)
         );
         $t .= '&nbsp;&nbsp;<a href="' . XH_hsc($url) . '" title="'
             . $plugin_tx['realblog']['tooltip_nextyear'] . '">'
@@ -161,13 +133,10 @@ class ArchiveView
     }
 
     /**
-     * Renders the monthly article lists.
-     *
-     * @param int $currentMonth The current month.
-     *
-     * @return string (X)HTML.
+     * @param int $currentMonth
+     * @return string
      */
-    protected function renderMonthlyArticleLists($currentMonth)
+    private function renderMonthlyArticleLists($currentMonth)
     {
         $t = '';
         for ($month = $currentMonth; $month >= 1; $month--) {
@@ -185,26 +154,22 @@ class ArchiveView
     }
 
     /**
-     * Renders an article list.
-     *
-     * @param array $articles An array of articles.
-     *
-     * @return string (X)HTML.
-     *
-     * @global string     The URL of the current page.
-     * @global array      The localization of the plugins.
-     * @global Controller The plugin controller.
+     * @param array<stdClass> $articles
+     * @return string
+     * @global string $su
+     * @global array $plugin_tx
+     * @global Controller $_Realblog_controller
      */
-    protected function renderArticleList($articles)
+    private function renderArticleList(array $articles)
     {
         global $su, $plugin_tx, $_Realblog_controller;
 
         $t = '<ul class="realblog_archive">';
         foreach ($articles as $article) {
             $url = $_Realblog_controller->url(
-                $su, $article->title, array(
-                    'realblogID' => $article->id
-                )
+                $su,
+                $article->title,
+                array('realblogID' => $article->id)
             );
             $t .= '<li>'
                 . date($plugin_tx['realblog']['date_format'], $article->date)
@@ -217,15 +182,12 @@ class ArchiveView
     }
 
     /**
-     * Renders the search results.
-     *
-     * @return string (X)HTML.
-     *
-     * @global string     The URL of the current page.
-     * @global array      The localization of the plugins.
-     * @global Controller The plugin controller.
+     * @return string
+     * @global string $su
+     * @global array $plugin_tx
+     * @global Controller $_Realblog_controller
      */
-    protected function renderSearchResults()
+    private function renderSearchResults()
     {
         global $su, $plugin_tx, $_Realblog_controller;
 
@@ -239,9 +201,9 @@ class ArchiveView
                 $currentMonth = $month;
             }
             $url = $_Realblog_controller->url(
-                $su, $article->title, array(
-                    'realblogID' => $article->id
-                )
+                $su,
+                $article->title,
+                array('realblogID' => $article->id)
             );
             $t .= '<p>'
                 . date($plugin_tx['realblog']['date_format'], $article->date)
@@ -252,5 +214,3 @@ class ArchiveView
         return $t;
     }
 }
-
-?>

commit fa9966de9ba8807d6a9b1805614c135708dffda8

Fix regression introduced with 891b322d

compat.php
@@ -36,7 +36,7 @@ function showrealblog($options = null, $category = 'all')
                 break;
         }
     }
-    return $_Realblog_controller->blog($includesearch, $realBlogCat);
+    return $_Realblog_controller->blog($includesearch, $category);
 }
 
 /**