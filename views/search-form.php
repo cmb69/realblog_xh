e037a6e057ab194b7add2dba25036cccb541a8c2
Christoph M. Becker <cmbecker69@gmx.de>
2017-02-03 14:20:37 +0100

    Close #37: Improve search-form.php
    We simplify the structure, and also change the localization of the
    search button to emphasize that this search is only for blog articles.

--- views/search-form.php
+++ views/search-form.php
@@ -1,9 +1,7 @@
 <!-- realblog search form -->
 <form class="realblog_search_form" method="get" action="<?=$this->actionUrl?>">
     <input type="hidden" name="selected" value="<?=$this->pageUrl?>">
-    <p>
-        <input type="text" name="realblog_search" size="15" class="realblog_search_input"
-               maxlength="64" title="<?=$this->text('search_hint')?>" placeholder="<?=$this->text('search_placeholder')?>">
-        <input type="submit" value="<?=$this->text('search_button')?>">
-    </p>
+    <input type="text" name="realblog_search" class="realblog_search_input"
+           title="<?=$this->text('search_hint')?>" placeholder="<?=$this->text('search_placeholder')?>">
+    <button><?=$this->text('search_button')?></button>
 </form>
