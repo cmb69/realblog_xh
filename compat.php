b06f53299a989884fc3ce4ed6f4562577fcfb988
Christoph M. Becker <cmbecker69@gmx.de>
2017-02-20 00:38:25 +0100

    Switch to GNU style file headers

--- compat.php
+++ compat.php
@@ -1,10 +1,24 @@
 <?php
 
 /**
- * @copyright 2006-2010 Jan Kanters
- * @copyright 2010-2014 Gert Ebersbach <http://ge-webdesign.de/>
- * @copyright 2014-2017 Christoph M. Becker <http://3-magi.net/>
- * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * Copyright 2006-2010 Jan Kanters
+ * Copyright 2010-2014 Gert Ebersbach
+ * Copyright 2014-2017 Christoph M. Becker
+ *
+ * This file is part of Realblog_XH.
+ *
+ * Realblog_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Realblog_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Realblog_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
