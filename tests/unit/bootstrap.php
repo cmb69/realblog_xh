7d7ac16aa97b7149d54294e0f588bf7c37de5d0a
Christoph M. Becker <cmbecker69@gmx.de>
2023-01-31 18:19:58 +0100

    Test MostPopularController

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -8,5 +8,6 @@ require_once "./classes/Finder.php";
 require_once "./classes/FeedController.php";
 require_once "./classes/FeedLinkController.php";
 require_once "./classes/MainController.php";
+require_once "./classes/MostPopularController.php";
 require_once "./classes/ScriptEvaluator.php";
 require_once "./classes/View.php";
