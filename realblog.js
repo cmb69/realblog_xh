b58499a24b1d9142078a9128a102b095c196952a
Christoph M. Becker <cmbecker69@gmx.de>
2023-02-26 00:25:00 +0100

    Drop JS support for date/time picker
    jscalendar is removed as of 90edd3f, so there is no point in having
    supporting JS.

--- realblog.js
+++ realblog.js
@@ -24,40 +24,6 @@
  */
 var REALBLOG = REALBLOG || {};
 
-/**
- * Initializes the date pickers.
- */
-REALBLOG.initDatePickers = function () {
-    var i, datePicker;
-
-    for (i = 1; i <= 3; i++) {
-        datePicker = document.getElementById("realblog_trig_date" + i);
-        if (datePicker) {
-            if (this.hasNativeDatePicker) {
-                datePicker.style.display = "none";
-            } else {
-                document.getElementById("realblog_date" + i).onfocus = function () {
-                    this.blur();
-                };
-                Calendar.setup({
-                    inputField: "realblog_date" + i,
-                    ifFormat: "%Y-%m-%d",
-                    button: "realblog_trig_date" + i,
-                    align: "Br",
-                    singleClick: true,
-                    firstDay: 1,
-                    weekNumbers: false,
-                    electric: false,
-                    showsTime: false,
-                    timeFormat: "24"
-                });
-            }
-        }
-    }
-}
-
-REALBLOG.initDatePickers();
-
 (function () {
     var select, i, option;
 
