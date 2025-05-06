067d2953d5272c52687feef84562bbacf1c467d2
Christoph M. Becker <cmbecker69@gmx.de>
2025-05-06 16:02:53 +0200

    Tweak article edit form
    This is still not great, but looks a bit better now.

--- views/article_form.php
+++ views/article_form.php
@@ -45,9 +45,9 @@ if (!defined("CMSIMPLE_XH_VERSION")) {http_response_code(403); exit;}
     <input type="hidden" name="realblog_token" value="<?=$this->esc($csrfToken)?>">
     <table>
       <tr>
-        <td><label for="realblog_date1" class="realblog_label"><?=$this->text('date_label')?></label></td>
-        <td><label for="realblog_date2" class="realblog_label"><?=$this->text('startdate_label')?></label></td>
-        <td><label for="realblog_date3" class="realblog_label"><?=$this->text('enddate_label')?></span></label>
+        <th><label for="realblog_date1" class="realblog_label"><?=$this->text('date_label')?></label></td>
+        <th><label for="realblog_date2" class="realblog_label"><?=$this->text('startdate_label')?></label></td>
+        <th><label for="realblog_date3" class="realblog_label"><?=$this->text('enddate_label')?></span></label>
       </tr>
       <tr>
         <td>
@@ -71,8 +71,8 @@ if (!defined("CMSIMPLE_XH_VERSION")) {http_response_code(403); exit;}
         </td>
       </tr>
       <tr>
-        <td><label for="realblog_status" class="realblog_label"><?=$this->text('label_status')?></label></td>
-        <td colspan="2"></td>
+        <th><label for="realblog_status" class="realblog_label"><?=$this->text('label_status')?></label></td>
+        <th colspan="2"></td>
       </tr>
       <tr>
         <td>
@@ -97,18 +97,24 @@ if (!defined("CMSIMPLE_XH_VERSION")) {http_response_code(403); exit;}
           </label>
         </td>
       </tr>
+      <tr>
+        <th colspan="3"><label for="realblog_categories" class="realblog_label"><?=$this->text('label_categories')?></label></td>
+      </tr>
+      <tr>
+        <td colspan="2"><input type="text" id="realblog_categories" name="realblog_categories" value="<?=$this->esc($categories)?>" size="50"></td>
+        <td>
+          <select id="realblog_category_select">
+            <option><?=$this->text('label_category_add')?></option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <th colspan="3"><label for="realblog_title" class="realblog_label"><?=$this->text('title_label')?></label></td>
+      </tr>
+      <tr>
+        <td colspan="3"><input type="text" id="realblog_title" name="realblog_title" value="<?=$this->esc($title)?>" size="50"></td>
+      </tr>
     </table>
-    <p>
-      <label for="realblog_categories" class="realblog_label"><?=$this->text('label_categories')?></label>
-      <input type="text" id="realblog_categories" name="realblog_categories" value="<?=$this->esc($categories)?>" size="50">
-      <select id="realblog_category_select">
-        <option><?=$this->text('label_category_add')?></option>
-      </select>
-    </p>
-    <p>
-      <label for="realblog_title" class="realblog_label"><?=$this->text('title_label')?></label>
-      <input type="text" id="realblog_title" name="realblog_title" value="<?=$this->esc($title)?>" size="50">
-    </p>
     <p>
       <label for="realblog_headline" class="realblog_label"><?=$this->text('headline_label')?></label>
       <textarea class="realblog_headline_field" id="realblog_headline" name="realblog_headline" rows="6" cols="60"><?=$this->esc($teaser)?></textarea>
